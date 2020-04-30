b7f3eb9e5be6e4c0d89aa26a6368f3a10068c1de
Christopher Sullivan <S00188845@mail.itsligo.ie>
2020-04-30 13:14:28 +0100

    Add project files.

---
+++ Darts League/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Darts_League
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    
+    public partial class MainWindow : Window
+    {
+        DartsdatabaseEntities db = new DartsdatabaseEntities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayerCombo_Loaded(object sender, RoutedEventArgs e)
+        {
+            var query = from a in db.Players
+                        select a.Name;
+
+            PlayerCombo.ItemsSource = query.ToList();
+        }
+
+        private void PlayerCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string Stats = (string)PlayerCombo.SelectedItem;
+
+            if (Stats != null)
+            {
+                var query = from a in db.Players
+                            select new
+                            {
+                                Name = a.Name,
+                                Age = a.Age,
+                                Nationality = a.Nationality,
+                                Debut = a.Debut
+                            };
+
+                PlayersStatsdg.ItemsSource = query.ToList();
+            }
+        }
+
+        private void PlayerNameCombo2_Loaded(object sender, RoutedEventArgs e)
+        {
+            var query = from a in db.Players
+                        select a.Name;
+
+            PlayerNameCombo2.ItemsSource = query.ToList();
+        }
+    }
+}
